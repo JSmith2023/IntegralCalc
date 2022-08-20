e7fd47466b47b469291bf8d5216b1266f53e0c2f
JSmith2023 <111526652+JSmith2023@users.noreply.github.com>
2022-08-20 10:14:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for the app to be ready
 app.on('ready', function(){
@@ -16,4 +17,84 @@ app.on('ready', function(){
         slashes: true
 
     })); 
+//Quit app when closed
+mainWindow.on('closed', function(){
+    app.quit();
 });
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+});
+// Handle create add Menu(for variable selection)
+function createAddWindow(){
+    //create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: "Select a Variable for the Calculation"
+    });
+    //load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    //Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    })
+};
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu: [
+            {
+                label: 'Select Variable',
+                accelerator: process.platform == 'darwin' ? 'Command+S': 'Ctrl+S',
+click(){
+    createAddWindow();
+}
+            },
+            {
+                label: 'Definite Integral'
+            },
+            {
+                label: 'Clear',
+                accelerator: process.platform == 'darwin' ? 'Command+W': 'Ctrl+W',
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q': 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            },
+        ]
+    }
+];
+
+//if mac add ampty obj to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I': 'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+            role: 'reload'
+            }
+        ]
+    });
+}
